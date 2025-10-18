3be7265a46c7b4e801622bfc3216bad48dbf6807
Chandresh202004 <sambitjena438@gmail.com>
2025-10-18 21:14:56 +0530

    Hashing_01

---
+++ Hashing_01.py
@@ -0,0 +1,29 @@
+a = [1,3,5,3,6,7,2,9,10,4,7,9,8]
+b = [7,2,8,3,4,2,11,4,5,1,3,1,9,8,7]
+
+
+
+#for i in range(0,n):
+#    count= 0
+#    for j in range(0,m):
+#        if m[i]==n[j]:
+#            count+=1
+#    print(f'the element {a[i]} occurs {count} in b')
+#
+#
+#for i in a:
+#    count = 0
+#    for j in b:
+#        if i == j:
+#            count+=1 
+#print(count)
+
+hash_list = [0]*11
+
+for num in a:
+    hash_list[num]+=1
+    for num in b:
+        if num<1 or num>10:
+            print(0)
+        else:
+            print(hash_list[num])
