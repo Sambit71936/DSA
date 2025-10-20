ef423f2b7ef3f5dd76b26461fef4d6ae9fcbe90c
Chandresh202004 <sambitjena438@gmail.com>
2025-10-20 11:43:41 +0530

    Recursion Theoruy 02

---
+++ Head_Recursion.py
@@ -0,0 +1,40 @@
+global count
+count = 0
+def func():
+    if (count == 4):
+        return
+    print("Hello")
+    count+=1
+    func()
+func()
+
+def func():
+    if (count == 4):
+        return
+    print("Hello")
+    count+=1
+    func()
+
+def func():
+    if (count == 4):
+        return
+    print("Hello")
+    count+=1
+    func()
+
+def func():
+    if (count == 4):
+        return
+    print("Hello")
+    count+=1
+    func()
+
+def func():
+    if (count == 4):
+        return
+    print("Hello")
+    count+=1
+    func()
+
+
+
