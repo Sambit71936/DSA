32065bda31b1cded05ef01c1309f5e136e585e50
Chandresh202004 <sambitjena438@gmail.com>
2025-10-20 11:44:00 +0530

    Recursion Theoruy 03

---
+++ Tail_Recursion.py
@@ -0,0 +1,35 @@
+count = 0
+
+def func():
+    global count
+    if count == 4:
+        return
+    count += 1
+    func()
+    print("hello")
+func()
+    
+
+def func():
+    global count
+    if count == 4:
+        return
+    count += 1
+    func()
+    print("hello")
+
+def func():
+    global count
+    if count == 4:
+        return
+    count += 1
+    func()
+    print("hello")
+
+def func():
+    global count
+    if count == 4:
+        return
+    count += 1
+    func()
+    print("hello")
