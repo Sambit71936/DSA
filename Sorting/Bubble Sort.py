27f565c3363e53e116753adc879822aa62512c85
Chandresh202004 <sambitjena438@gmail.com>
2025-12-23 20:09:12 +0530

    DSA

---
+++ Sorting/Bubble Sort.py
@@ -0,0 +1,12 @@
+arr = [23, 12, 45, 67, 34, 89, 10]
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n-2,-1,-1):
+        for j in range(0,i+1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+    return arr
+
+
+print(bubble_sort(arr))
