a2fc167824d6f5a2933804aef2b0644764350fef
Chandresh202004 <sambitjena438@gmail.com>
2025-12-25 11:48:51 +0530

    DSA

---
+++ Array/Second Largest.py
@@ -0,0 +1,11 @@
+arr = list(map(int,input().split()))
+
+first = second = float('-inf')
+
+for i in range(0,len(arr)):
+    if arr[i]>first:
+        first = arr[i]
+    for i in range(0,len(arr)):
+        if arr[i]>second and arr[i]<first:
+            second = arr[i]
+print(second)
