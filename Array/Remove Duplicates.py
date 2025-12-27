cd9d1bbd9e02627931d0939741f0b34fafed2176
Chandresh202004 <sambitjena438@gmail.com>
2025-12-27 20:43:11 +0530

    DSA

--- Array/Remove Duplicates.py
+++ Array/Remove Duplicates.py
@@ -1,2 +1,12 @@
 arr = [1,1,2,3,4,4,5,6,6,6,7,7,8,8,8,8,9,9,9,9]
 
+def remove_duplicates(arr):
+    unique_arr = []
+    for i in arr:
+        if i not in unique_arr:
+            unique_arr.append(i)
+    return unique_arr
+
+print(remove_duplicates(arr))
+
+# Time Complexity: O(n^2)
